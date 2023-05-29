6110972a3879618a6a5ae1186449c00d587f9630
Bruno Duarte <brunoduarte96@outlook.com>
2023-05-29 19:59:25 -0300

    hateoas na TripulacaoController

--- src/main/java/com/duarte/onePieceAPI/controller/TripulacaoController.java
+++ src/main/java/com/duarte/onePieceAPI/controller/TripulacaoController.java
@@ -3,6 +3,9 @@ package com.duarte.onePieceAPI.controller;
 import com.duarte.onePieceAPI.dto.TripulacaoDTO;
 import com.duarte.onePieceAPI.service.TripulacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,24 +14,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-    @RequestMapping("/api/v1/tripulacao")
+@RequestMapping("/api/v1/tripulacao")
 public class TripulacaoController {
 
     @Autowired
-   private TripulacaoService tripulacaoService;
+    private TripulacaoService tripulacaoService;
 
 
     @Transactional
     @GetMapping
-    public List<TripulacaoDTO>listarTripulacao(){
-        return tripulacaoService.listarTripulacoes();
+    public List<EntityModel<TripulacaoDTO>> listarTripulacao() {
+        List<TripulacaoDTO> tripulacoes = tripulacaoService.listarTripulacoes();
+
+        return tripulacoes.stream()
+                .map(tripulacao -> {
+                    String nome = tripulacao.getNome();
+
+                    Link buscarTripulacaoLink = WebMvcLinkBuilder.linkTo(TripulacaoController.class)
+                            .slash(nome)
+                            .withRel("buscarTripulacao");
+
+                    return EntityModel.of(tripulacao, buscarTripulacaoLink);
+                })
+                .collect(Collectors.toList());
     }
 
     @Transactional
     @GetMapping("/{nome}")
-    public List<Object>listarTripulacaoPorNome(@PathVariable("nome")String nome) {
+    public List<Object> listarTripulacaoPorNome(@PathVariable("nome") String nome) {
         return Collections.singletonList(tripulacaoService.buscarTripulacaoPorNome(nome));
     }
 
